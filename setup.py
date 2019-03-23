553e647fe3d8a4b47dc9e1b9c626356c09d47cb6
Ilya Oshchepkov <ilya.oshchepkov@gmail.com>
2019-03-23 21:04:25 +0300

    simplify Sphinx support

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import sys
 from setuptools import setup, find_packages
 
 import versioneer
 
-# Get the long description from the README file
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                       'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+with open('README.rst') as readme_file:
+    long_description = readme_file.read()
 
 name = 'pygeoid'
 version = versioneer.get_version()
-
-#cmdclass = {
-#    'build_sphinx': BuildDoc,
 cmdclass = versioneer.get_cmdclass()
-#}
+
+install_requires = ['numpy', 'scipy', 'pyproj',
+                    'pint', 'pyshtools', 'joblib'],
 
 setup(name=name,
       version=version,
@@ -46,11 +41,10 @@ setup(name=name,
           'Topic :: Education',
           'Topic :: Scientific/Engineering',
       ],
-      keywords=['geodesy', 'gravimetry', 'geoid'],
+      keywords=['geodesy', 'gravimetry', 'geoid', 'gravity'],
       packages=find_packages(),
       include_package_data=True,
       tests_require=['pytest'],
       setup_requires=['pytest-runner'],
-      install_requires=['numpy', 'scipy', 'pyproj', 'pint', 'pyshtools',
-          'joblib'],
+      install_requires=install_requires,
       python_requires='>=3.5')
